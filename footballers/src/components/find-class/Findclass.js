7db4f0f7f247c6e39b1d8ba0c22647b405459f71
Muhammad Zahid Iqbal <56752369+Muhammad-Zahid-Iqbal@users.noreply.github.com>
2023-11-17 08:36:37 +0500

    update

--- footballers/src/components/find-class/Findclass.js
+++ footballers/src/components/find-class/Findclass.js
@@ -1,33 +1,49 @@
-import { Box, Button, Grid, Typography } from '@mui/material'
-import React from 'react'
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import TextField from '@mui/material/TextField';
-import SearchIcon from '@mui/icons-material/Search';
+import { Box, Button, Grid, Typography } from "@mui/material";
+import React from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Findclass = () => {
-  const [findClass, setFindClass] = React.useState('');
+  const [findClass, setFindClass] = React.useState("");
 
   const handleChange = (event) => {
     setFindClass(event.target.value);
   };
+  
   return (
     <>
-      <Box sx={{ background: "#f2f2f2", }}>
+      <Box sx={{ background: "#f2f2f2" }}>
         <Grid container sx={{ minHeight: "380px" }}>
-          <Grid item sm={12} sx={{ display: "flex", justifyContent: "center", margin: "auto" }}>
-            <h1 style={{fontSize:"36px", paddingTop:"20px"}}>Find A Class</h1>
-            
+          <Grid
+            item
+            sm={12}
+            sx={{ display: "flex", justifyContent: "center", margin: "auto" }}
+          >
+            <h1 style={{ fontSize: "36px", paddingTop: "20px" }}>
+              Find A Class
+            </h1>
           </Grid>
-         
-          <Grid container  sx={{ display: "flex", justifyContent:"center" }}>
-          <Grid item sm ={12} xs={12}>
-          <Typography sx={{position:"relative", left:"17%", top:"40%",  paddingBottom:"20px", fontSize:"18px"}}>Choose your location</Typography>
-          </Grid>
-            <Grid item sm={2} xs={12}>
-              <FormControl fullWidth >
+
+          <Grid container sx={{ display: "flex", justifyContent: "center" }}>
+            <Grid item sm={12} xs={12}>
+              <Typography
+                sx={{
+                  position: "relative",
+                  left: "17%",
+                  top: "40%",
+                  paddingBottom: "20px",
+                  fontSize: "18px",
+                }}
+              >
+                Choose your location
+              </Typography>
+            </Grid>
+            <Grid item sm={1.8} xs={12}>
+              {/* <FormControl fullWidth >
                 <InputLabel id="demo-simple-select-label">Class</InputLabel>
                 <Select
                   labelId="demo-simple-select-label"
@@ -40,14 +56,20 @@ const Findclass = () => {
                   <MenuItem value={20}>Class B</MenuItem>
                   <MenuItem value={30}>Class C</MenuItem>
                 </Select>
-              </FormControl>
+              </FormControl> */}
+              <select id="cars" name="cars" style={{ width: '100%',minWidth: '202px', height: '30px', padding: '27px'}}>
+                <option style={{fontSize:"20px"}} value="volvo">Class A</option>
+                <option style={{fontSize:"20px"}} value="saab">Class B</option>
+                <option style={{fontSize:"20px"}} value="mercedes">Class C</option>
+                <option style={{fontSize:"20px"}} value="audi">Class D</option>
+              </select>
             </Grid>
-            <Grid item sm={5} xs={12} >
+            <Grid item sm={5} xs={12}>
               <TextField
-              fullWidth
+                fullWidth
                 id="search"
                 // label="Search"
-                placeholder='Type to enter your address, town or postcode'
+                placeholder="Type to enter your address, town or postcode"
                 // variant="outlined"
                 InputProps={{
                   startAdornment: (
@@ -55,20 +77,28 @@ const Findclass = () => {
                       <SearchIcon color="action" />
                     </>
                   ),
-                  
-                  
                 }}
                 // sx={{ border: "1px solid #ff7002", borderRadius:"10px"}}
               />
             </Grid>
-            <Grid item sm={1}  xs={12}>
-              <Button sx={{width:"56px", height:"56px", background:"#ff7002", color:"#fff"}} variant="contained">Go</Button>
+            <Grid item sm={1} xs={12}>
+              <Button
+                sx={{
+                  width: "56px",
+                  height: "56px",
+                  background: "#ff7002",
+                  color: "#fff",
+                }}
+                variant="contained"
+              >
+                Go
+              </Button>
             </Grid>
           </Grid>
         </Grid>
       </Box>
     </>
-  )
-}
+  );
+};
 
-export default Findclass
+export default Findclass;
