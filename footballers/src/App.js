ae66d3f0b2f38244b8376b26cfc74bcdce47f173
Muhammad Zahid Iqbal <56752369+Muhammad-Zahid-Iqbal@users.noreply.github.com>
2023-11-17 10:19:07 +0500

    update

--- footballers/src/App.js
+++ footballers/src/App.js
@@ -5,12 +5,14 @@ import Appbarlogin from "./components/AppbarLogin/Appbarlogin";
 import FindClass from "./components/find-class";
 import Locationtext from "./components/white-area/Locationtext";
 import Footer from "./components/footer-area/Footer";
+import Counselling from "./components/Counselling-London/Counselling";
 
 function App() {
   return (
     <Layout>
       <Appbarlogin />
-      <FindClass/>
+      {/* <FindClass/> */}
+      <Counselling/>
       <Locationtext/>
       <Footer />
     </Layout>
